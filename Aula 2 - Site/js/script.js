561cc499bcaa70ea38c4ef5c4d21074cf6a76190
felipebelchiorb <belchiorbaliza@gmail.com>
2026-09-10 10:49:25 -0300

    ababuble

---
+++ Aula 2 - Site/js/script.js
@@ -0,0 +1,54 @@
+//comentário de uma linha
+
+/* esse comentário tem várias linhas
+olha que doido
+isso aqui
+*/
+
+// nossa ferramenta de debug, o console.log
+console.log("olá mundo") 
+
+//declarando váriaveis e constantes
+let userId = 1234
+let userName = "Mineirinho"
+let isJumping = false
+let inventory = ["chave", "lanterna"]
+let buff
+
+const npcName = "Astolfo"
+
+/*console.log(`Id do usuário: ${userId}`)
+console.log(`Nome do usuário: ${userName}`)
+console.log(`Usuário pulando: ${isJumping}`)
+console.log(inventory)
+console.log(buff)
+console.log(npcName)
+*/
+
+// usando operadores
+
+// operadores aritméricos
+
+let numberOne = 10
+let numerTwo = 50
+let result
+
+// soma
+result = (numberOne + numerTwo)
+console.log(`${numerOne} + ${numerTwo} = ${result}`)
+
+// subtração
+result = (numberOne - numerTwo)
+console.log(`${numerOne} - ${numerTwo} = ${result}`)
+
+// divisão
+result = (numberOne / numerTwo)
+console.log(`${numerOne} / ${numerTwo} = ${result}`)
+
+// módulo
+result = (numberOne % numerTwo)
+console.log(`${numerOne} % ${numerTwo} = ${result}`)
+
+// potência
+result = (numberOne ** numerTwo)
+console.log(`${numerOne} ** ${numerTwo} = ${result}`)
